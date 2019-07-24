2c89d082539b9bbe9af610fa895792de5d5ba7f2
jkchen-hub <1054109772@qq.com>
2019-07-24 17:02:05 +0800

    changed

--- src/main/java/com/demo/sprBootGenGroovyJenfile/controller/sprBootGenGroovyJenfileCon.java
+++ src/main/java/com/demo/sprBootGenGroovyJenfile/controller/sprBootGenGroovyJenfileCon.java
@@ -1,15 +1,22 @@
 package com.demo.sprBootGenGroovyJenfile.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.sprBootGenGroovyJenfile.entity.UserVO;
 
 @RestController
 public class sprBootGenGroovyJenfileCon {
 	
-	@RequestMapping("/login")
-	public String logindsss() {
-//		return "pleas login";
-		return "index.html";
-	}
-	
+    @RequestMapping("welcome")
+    public String welcome(UserVO user){
+        System.out.println("POJO: " + user.getClass().getName() + 
+                ", hash code: " + user.hashCode() + ", " + user.toString());
+        System.out.println(user.getUsername());
+        System.out.println(user.getPassword());
+        return "redirect:/";
+        
+    }
 }
---
+++ src/main/java/com/demo/sprBootGenGroovyJenfile/entity/UserVO.java
@@ -0,0 +1,30 @@
+package com.demo.sprBootGenGroovyJenfile.entity;
+
+public class UserVO {
+
+    public String username;
+    public String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "username: " + username + ", password: " + password;
+    }
+
+}
+
